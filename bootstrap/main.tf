cef8eb9a28c31de820e9f6378b34b567536978ac
winterconquest <baselurker@gmail.com>
2026-08-10 13:00:09 +0900

    bootstrap/main.tf provider 추가

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,6 +1,16 @@
 
 #provider, data.aws_caller_identity
 #state 버킷 3종
+terraform {
+  required_version = ">= 1.11"
+  required_providers {
+    aws = { source = "hashicorp/aws", version = "~> 5.0" }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
 
 data "aws_caller_identity" "current" {}
 
